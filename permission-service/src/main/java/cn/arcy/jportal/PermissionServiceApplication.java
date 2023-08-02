0cdc2dcde17bc6c626b47ab2df4559d7b64a2530
arcy <arcy@dev.dop>
2023-08-02 18:35:45 +0800

    init(*):初始化框架

---
+++ permission-service/src/main/java/cn/arcy/jportal/PermissionServiceApplication.java
@@ -0,0 +1,7 @@
+package cn.arcy.jportal;
+
+public class PermissionServiceApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ user-service/src/main/java/cn/arcy/jportal/UserApiApplication.java
@@ -0,0 +1,7 @@
+package cn.arcy.jportal;
+
+public class UserApiApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
