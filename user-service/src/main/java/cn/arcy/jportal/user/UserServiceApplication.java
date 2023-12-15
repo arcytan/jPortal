fc2617fa9c503d934d92960e90ddb8d4f8e66825
arcy <arcy@ctstudio.cn>
2023-12-15 23:34:03 +0800

    feat(权限):增加权限相关数据表

--- user-service/src/main/java/cn/arcy/jportal/user/UserServiceApplication.java
+++ user-service/src/main/java/cn/arcy/jportal/user/UserServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class UserServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class);
     }
