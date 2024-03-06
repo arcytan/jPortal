3ab66cd0b3c93091a72b439a19a1535062359b23
arcy <arcy@ctstudio.cn>
2024-03-06 18:26:55 +0800

    feat(软删除): 临时提交

--- boot-starter/jportal-boot-autoconfigure/src/main/java/cn/arcy/jportal/jpa/repository/SimpleJpaRepositoryWithSoftDelete.java
+++ boot-starter/jportal-boot-autoconfigure/src/main/java/cn/arcy/jportal/jpa/repository/SimpleJpaRepositoryWithSoftDelete.java
@@ -2,9 +2,14 @@ package cn.arcy.jportal.jpa.repository;
 
 import cn.arcy.jportal.jpa.entity.SoftDeleteEntity;
 import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 public class SimpleJpaRepositoryWithSoftDelete<T extends SoftDeleteEntity, ID> extends SimpleJpaRepository<T, ID> {
 
@@ -20,9 +25,27 @@ public class SimpleJpaRepositoryWithSoftDelete<T extends SoftDeleteEntity, ID> e
         this.entityManager = em;
     }
 
+    @Transactional
     @Override
     public long delete(Specification<T> spec) {
-        findOne(spec);
+        List<T> lists = findAll(spec);
+        if (lists.isEmpty()) {
+            return 0L;
+        }
+
         return super.delete(spec);
+    }
+
+    @Transactional
+    public void softDelete(T entity)
+    {
+        entity.setDeleted(true);
+        entity.setDeletedAt(LocalDateTime.now());
+        save(entity);
+    }
+
+    public long softDelete(List<T> entities)
+    {
+        return 1L;
     }
 }
